944af5d9138f0588c3b9090d00230f2c9f84b755
Vienta <vienta.su@gmail.com>
2013-03-07 09:43:29 +0800

    Initial Commit

---
+++ PokerChat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PokerChat
+//
+//  Created by 束 永兴 on 13-3-7.
+//  Copyright (c) 2013年 Vienta.su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PokerChat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PokerChat
+//
+//  Created by 束 永兴 on 13-3-7.
+//  Copyright (c) 2013年 Vienta.su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
