fde47508f4dfd683896ecee1a7d412f1b65d2253
Vienta.su <vienta.su@gmail.com>
2013-03-25 14:09:29 +0800

    fix register spinner

--- PokerChat/UI/RegisterView.m
+++ PokerChat/UI/RegisterView.m
@@ -72,6 +72,7 @@
 - (void)close
 {
     [super close];
+    [self.spinner removeFromSuperview];
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
 }
